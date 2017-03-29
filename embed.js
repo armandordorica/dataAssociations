fbafb687eedf49ffd2a6bf2a88c82b3fd2e97072
Armando Ordorica <armandordorica@gmail.com>
2017-03-29 22:55:43 +0000

    first commit

--- embed.js
+++ embed.js
@@ -1,17 +1,6 @@
 var mongoose  = require("mongoose");
 mongoose.connect("mongodb://localhost/blog_demo");
 
-//USER 
-//Create the schema
-var userSchema = new mongoose.Schema({
-    email: String, 
-    name: String
-});
-
-//compile schema into a model
-var User = mongoose.model("User", userSchema);
-
-
 
 //POST
 //Create the schema
@@ -23,28 +12,52 @@ var postSchema = new mongoose.Schema({
 //compile the schema into a model
 var Post = mongoose.model("Post", postSchema);
 
-// var newUser = new User({
-//     email: "charlie@brown.edu", 
-//     name: "Charlie Brown"
-// }); 
 
-// newUser.save(function(err, user){
+
+//USER 
+//Create the schema
+var userSchema = new mongoose.Schema({
+    email: String, 
+    name: String,
+    posts: [postSchema]
+});
+
+//compile schema into a model
+var User = mongoose.model("User", userSchema);
+
+
+
+
+
+var newUser = new User({
+    email: "hermione@hogwarts.edu", 
+    name: "Hermione Granger"
+}); 
+
+newUser.posts.push({
+    title: "How to brew polyjuice potions", 
+    content: "Just kidding, go to potions class to learn it"
+});
+
+newUser.save(function(err, user){
+    if(err){
+        console.log(err); 
+    }else{
+        console.log(user);
+    }
+});
+
+// var newPost = new Post({
+//     title: "Reflections on Apples", 
+//     content: "They are delicious"
+// });
+
+// newPost.save(function(err, post){
 //     if(err){
-//         console.log(err); 
+//         console.log(err);
 //     }else{
-//         console.log(user);
+//         console.log(post);
 //     }
 // });
 
-var newPost = new Post({
-    title: "Reflections on Apples", 
-    content: "They are delicious"
-});
 
-newPost.save(function(err, post){
-    if(err){
-        console.log(err);
-    }else{
-        console.log(post);
-    }
-});
